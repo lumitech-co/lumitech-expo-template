8c33dd916f5ffdba231b92101ff53b5af764f7ab
Andrei <andrei@MacBook-Pro-Andrei.local>
2025-07-07 11:08:55 +0300

    feat: removed unused aliases

--- src/app/(auth)/index.tsx
+++ src/app/(auth)/index.tsx
@@ -1,12 +1,16 @@
 import { Box, Text } from "ui";
 import React from "react";
+import { AuthHeader } from "widgets";
 
 export default function AuthScreen() {
   return (
-    <Box flex={1} alignItems="center" justifyContent="center">
+    <Box>
+      <AuthHeader />
+      <Box flex={1} alignItems="center" justifyContent="center">
       <Text color="black" fontSize="title" fontWeight={500} fontFamily="Bold">
         Auth screen
       </Text>
+    </Box>
     </Box>
   );
 }
